f9f3373fc679b9e5690f415ffe142fbff7d4aace
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-06 10:41:18 +0300

    new read me file was updated and perfomance.sql was created

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,64 @@
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.user_id
+JOIN 
+    properties p ON b.property_id = p.property_id
+JOIN 
+    payments pay ON b.booking_id = pay.booking_id;
+
+
+EXPLAIN ANALYZE 
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name,
+    p.location,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.user_id
+JOIN 
+    properties p ON b.property_id = p.property_id
+JOIN 
+    payments pay ON b.booking_id = pay.booking_id;
+
+
+SELECT 
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id,
+    p.name AS property_name
+FROM 
+    bookings b
+JOIN 
+    users u ON b.user_id = u.user_id
+JOIN 
+    properties p ON b.property_id = p.property_id
+WHERE 
+    b.user_id = 'specific_user_id';  -- Filter for a specific user
