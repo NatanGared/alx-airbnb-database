b452d05c4af654e591f7c5fddae888f154db41ec
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-06 10:29:09 +0300

    new read me file was updated and aggregations_and_window_functions.sql was created

---
+++ database-adv-script/database_index.sql
@@ -0,0 +1,13 @@
+-- Index for User Table
+CREATE INDEX idx_user_email ON users(email);
+CREATE INDEX idx_user_id ON users(user_id);
+
+-- Index for Booking Table
+CREATE INDEX idx_booking_user_id ON bookings(user_id);
+CREATE INDEX idx_booking_property_id ON bookings(property_id);
+CREATE INDEX idx_booking_dates ON bookings(start_date, end_date);
+
+-- Index for Property Table
+CREATE INDEX idx_property_id ON properties(property_id);
+CREATE INDEX idx_property_name ON properties(name);
+CREATE INDEX idx_property_location ON properties(location);
---
+++ database-adv-script/index_performance.md
@@ -0,0 +1,4 @@
+Key indicators of improved performance include:
+Reduced cost value.
+Change in the rows estimated to be scanned.
+Use of indexes in the EXPLAIN output.
