942337f4d6f873bd726f65cc3a2adfcad0b3a1cf
NatanGared <188752465+NatanGared@users.noreply.github.com>
2025-07-06 10:06:28 +0300

    new read me file and join-queries.sql were created

---
+++ database-adv-script/README.md
@@ -0,0 +1,3 @@
+INNER JOIN: Retrieves only records with matching entries in both tables (users with bookings).
+LEFT JOIN: Retrieves all records from the left table (properties) and matching records from the right table (reviews), including those with no reviews.
+FULL OUTER JOIN: Retrieves all records from both tables, including those without matches (users without bookings and bookings without linked users).
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,36 @@
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
+FROM 
+    users u
+INNER JOIN 
+    bookings b ON u.user_id = b.user_id;
+
+SELECT 
+    p.property_id,
+    p.name AS property_name,
+    r.review_id,
+    r.rating,
+    r.comment
+FROM 
+    properties p
+LEFT JOIN 
+    reviews r ON p.property_id = r.property_id;
+
+SELECT 
+    u.user_id,
+    u.first_name,
+    u.last_name,
+    b.booking_id,
+    b.property_id,
+    b.start_date,
+    b.end_date
+FROM 
+    users u
+FULL OUTER JOIN 
+    bookings b ON u.user_id = b.user_id;
